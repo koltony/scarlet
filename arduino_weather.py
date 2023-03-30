ee7e787d3dc2c3e726e317f5b3f2254b017b20df
koltony <kolonics.antal@gmail.com>
2023-03-30 23:31:07 +0200

    handling wind speed calculations in python code

--- arduino_weather.py
+++ arduino_weather.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 import datetime as dt
 import diskcache
 from collections import defaultdict
-from typing import Optional, Dict
+from typing import Optional, Dict, List
 import statistics
 import log as log_
 
@@ -40,6 +40,11 @@ class ArduinoWeather(config.Component):
         self.cache_directory = None  # type: Optional[str]
         self.cache_max_age = None  # type: Optional[int]
         self.cache = None  # type: Optional[diskcache.Cache]
+        self.anemometer_milli_volt_out_min = None  # type: Optional[int]
+        self.anemometer_milli_volt_out_max = None  # type: Optional[int]
+        self.anemometer_max_meter_per_sec = None  # type:Optional[float]
+        self.arduino_max_milli_input_voltage = None  # type:Optional[float]
+        self.arduino_input_resolution = None  # type: Optional[int]
 
     def initialize(self):
         with diskcache.Cache(directory=self.cache_directory) as cache:
@@ -75,12 +80,28 @@ class ArduinoWeather(config.Component):
             log.warning('No Arduino Weather data found in cache')
             return None
 
+    def _value_to_wind_speed(self, value) -> float:
+        """"Converts arduino analog read value to actual wind speed"""
+        milli_volt_per_value = self.arduino_input_resolution / self.arduino_max_milli_input_voltage
+        delta_voltage = self.anemometer_milli_volt_out_max - self.anemometer_milli_volt_out_min
+        meter_per_sec_per_voltage = delta_voltage / self.anemometer_max_meter_per_sec  # mps/mV
+        value_voltage = value * milli_volt_per_value
+        if value_voltage < self.anemometer_milli_volt_out_min:
+            log.debug(f"voltage: {value_voltage} is under threshold: {self.anemometer_milli_volt_out_min} returning 0")
+            return 0.0
+
+        meter_per_sec = (value_voltage - self.anemometer_milli_volt_out_min) / meter_per_sec_per_voltage
+        km_per_hour = meter_per_sec * 3.6
+        log.debug(f"calculated wind speed: {km_per_hour}")
+        return round(km_per_hour, 2)
+
     def get_weather_data(self) -> Optional[Weather]:
         wind = mqtt.service.get_message('wind', clear_message=True)
         light = mqtt.service.get_message('light', clear_message=True)
         rain = mqtt.service.get_message('rain', clear_message=True)
         if wind is not None and light is not None:
-            weather = Weather(wind=float(wind) if float(wind) >= 0 else 0,
+            wind = self._value_to_wind_speed(float(wind))
+            weather = Weather(wind=wind,
                               light=float(light),
                               rain=bool(rain))
             self._cache_data(weather)
